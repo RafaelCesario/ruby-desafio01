d57aaa2425e071651841ccc25a5dcaa25ad98642
Rafael Cesario <rafaelcesario@gmail.com>
2023-12-11 22:57:32 -0300

    primeiro commit

---
+++ desafio.rb
@@ -0,0 +1,7 @@
+print "Digite o nome: "
+nome = gets.chomp
+print "Digite o sobrenome: "
+sobrenome = gets.chomp
+print "Digite a sua idade: "
+idade = gets.chomp
+puts "O nome completo é #{nome} #{sobrenome} e sua idade é #{idade} anos."
